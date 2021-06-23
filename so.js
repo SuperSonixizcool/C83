fa9e674ff627ab22764b34a3c9d5fde46eac2130
SuperSonixizcool <81672291+SuperSonixizcool@users.noreply.github.com>
2021-06-23 16:29:18 +0530

    Add files via upload

---
+++ so.js
@@ -0,0 +1,11 @@
+function names() {
+    var name1 = "Kanishk ";
+    var name2 = "Salian";
+    var res = name1.concat(name2)
+    document.getElementById("demo").innerHTML = res;
+}
+
+var number1 = 7789;
+var number2 = 17263;
+var add = number1 + number2;
+console.log(add);
